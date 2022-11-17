2ea4768a9958a20dc88f042ca647e657090f4ce0
nct2k3 <nguyennrdz@gmail.com>
2022-11-17 22:24:10 +0700

    updated

---
+++ ctjava.js
@@ -0,0 +1,7 @@
+alert("  RẤT VUI ĐƯỢC GẶP BẠN");
+var bn=document.querySelector('.qq');
+bn.classList.add('mau');
+setInterval(() => {
+    bn.classList.toggle('mau')
+    
+}, 500);
---
+++ meo.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<title> WEB MÈO NCT2K3</title>
+<link rel="stylesheet" href="thietkecss.css"/>	
+</head>
+
+<body>
+<p id="dong_transition"> </p>	
+<center id="mau">
+
+<b> <br/>
+  <p id="sizechu"> Chào mừng đến động mèo <br/>
+	<P id="size"> Nơi quy tụ bọn mèo chơi cỏ
+	  </P></P>
+	
+  <p><img src ="./meo.jpg" width="199"/>
+      <img src ="./MEO1.jpg" width="200"/><img src ="./MEO3.jpg" width="265"/><img src ="./MEO2.jpg" width="200"/> <br/><br/><br/>
+
+	  <br/>
+    <br/><br/><br/><br/>
+    <br/><br/><br/><br/><br/><br/><br/><br/>
+  </p>
+
+</center>
+	
+	
+</body>
+</html>
